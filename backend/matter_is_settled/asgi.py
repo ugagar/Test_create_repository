1d32416577c8fea56ae811acb532aa8629a2d524
Danil <ugarovd06@gmail.com>
2023-06-19 21:35:12 +0300

    Create base structure of project

---
+++ backend/matter_is_settled/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for matter_is_settled project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matter_is_settled.settings')
+
+application = get_asgi_application()
